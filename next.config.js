e52599b7020f633ff0bf6680578d7595c8d55b9d
Teddy Mwai <teddymwai11@gmail.com>
2023-03-12 22:01:14 +0300

    added links to images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "https://d33wubrfki0l68.cloudfront.net/7c2dda6eab52a4b95dc6c3503d5a09c4e314f756/4d7a5/images/placeholders/square1.svg",
+      "https://d33wubrfki0l68.cloudfront.net/6163c5a4083dab2763aa0f2aa9e6bded23630eb7/935d6/images/placeholders/square2.svg",
+      "https://d33wubrfki0l68.cloudfront.net/ded521c426f480d4e473a11836c6ab8e7e948c84/95877/images/placeholders/square3.svg",
+      "https://d33wubrfki0l68.cloudfront.net/07865c265551d7a67bdf88188ff62a07b84ffd6f/7e12f/images/placeholders/square4.svg",
+    ],
+  },
 }
 
 module.exports = nextConfig
