819d6f71b650310f76f163020b47b115020876eb
Teddy Mwai <teddymwai11@gmail.com>
2023-03-13 03:15:09 +0300

    added styles and created a util folder

---
+++ util/region.ts
@@ -0,0 +1,52 @@
+interface Region {
+  name: string;
+  amount: number;
+}
+
+const regions: Region[] = [
+  { name: "REGION 1: Karen, Dagoretti, Thindigua, Bomas,", amount: 400 },
+  { name: "REGION 2: Kinoo, Kikuyu, Uthiru, Muthiga,", amount: 400 },
+  {
+    name: "REGION 3: Langata, Roysambu, Zimmerman, Kasarani Police Post, General Motors, Junction Mall, Komarock, Umoja, Donholm, Buruburu,",
+    amount: 300,
+  },
+  {
+    name: "REGION 4: South C, Madaraka, Mbagathi, Muthaiga, Runda, Ridgeways, Yaya, Greenhouse, Prestige Ngong Road, Ngumba",
+    amount: 250,
+  },
+  { name: "REGION 5: Rongai, Ruiru, Ngong, Syokimau,", amount: 600 },
+  {
+    name: "REGION 6: Westlands, Upperhill, Parklands, Ngara, South B, Industrial Area, Nyayo Stadium, Nairobi West, Hurligham, State house.",
+    amount: 250,
+  },
+  {
+    name: "Loresho, Red Hill Road, UON Lower Kabete, All-Soaps, Survey, NYS, Utalii",
+    amount: 300,
+  },
+  {
+    name: "REGION 8: Fedha, Imara Daima, Nyayo Estate, Umoja 2, Mama-Lucy, Kaboel, Tajmall",
+    amount: 400,
+  },
+  {
+    name: "RIFT VALLEY: Eldoret, Kitale, Kericho, Kapenguria, Moi's Brigde, Soi, Kapsabet, Molo, Njoro, Narok, Naivasha, Nakuru, Bomet,",
+    amount: 400,
+  },
+  {
+    name: "WESTERN REGION: Busia, Mumias, Butere, Bungoma, Butere, Chavakali, Kilingili, Kimilili, Kakamega, Malana, Isibania, Mumias",
+    amount: 400,
+  },
+  {
+    name: "NYANZA REGION: Kisii, Kisumu, Nyamira, Rongo, Homa Bay, Kebirigo, Kendu Bay, Keroka, Maseno, Siaya, Kondele, Migori, Muhoroni",
+    amount: 400,
+  },
+  {
+    name: "CENTRAL REGION: Limuru, Nyahururu, Solai, Githunguri, Othaya, Nkubu, Karatina, Kerugoya, Muranga, Ol Kalau, Nyeri, Nanyuki, Embu",
+    amount: 400,
+  },
+  {
+    name: "COAST REGION; Diane, Malindi, Mombasa, Nyali, Ukunda, Viwandani, Voi, Lame, Kibwezi, Kilifi, Mito Andei, Wundanyi, Mwatate,Kwale",
+    amount: 400,
+  },
+];
+
+export default regions;
