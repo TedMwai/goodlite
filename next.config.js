12278ca28f08b418c89c8435c82ae1deb3e6136b
Teddy Mwai <teddymwai11@gmail.com>
2023-03-24 02:20:05 +0300

    added prisma & image resource

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
       "https://d33wubrfki0l68.cloudfront.net/6163c5a4083dab2763aa0f2aa9e6bded23630eb7/935d6/images/placeholders/square2.svg",
       "https://d33wubrfki0l68.cloudfront.net/ded521c426f480d4e473a11836c6ab8e7e948c84/95877/images/placeholders/square3.svg",
       "https://d33wubrfki0l68.cloudfront.net/07865c265551d7a67bdf88188ff62a07b84ffd6f/7e12f/images/placeholders/square4.svg",
+      "res.cloudinary.com",
     ],
   },
 }
