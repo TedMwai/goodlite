305f280be64ed31a37a7f07517adbe422f9f2f8e
Teddy Mwai <teddymwai11@gmail.com>
2023-03-27 22:16:34 +0300

    Added authentication & refactored navbar code

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,8 @@ import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import { FiSearch, FiMenu } from "react-icons/fi";
 import { useShop } from "@/context/context";
+import { useSession, signIn, signOut } from "next-auth/react";
+
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -11,25 +13,82 @@ const montserrat = Montserrat({
 });
 
 const Navbar: FC = () => {
-  const { setCartOpen, isMobile, setIsMobile, setSideNav } = useShop();
-
-  useEffect(() => {
-    if (window.innerWidth <= 991) {
-      setIsMobile(true);
-    }
-  }, [setIsMobile]);
+  const { setCartOpen, setSideNav } = useShop();
+  const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(session);
 
   return (
     <div className={`${montserrat.className} w-full mx-auto bg-white border-b`}>
-      {isMobile ? (
-        <div className="flex justify-between items-center w-full p-5 bg-white">
-          <div onClick={() => setSideNav(true)}>
-            <FiMenu className="text-2xl cursor-pointer" />
+      <div className="flex justify-between items-center w-full p-5 bg-white lg:hidden">
+        <div onClick={() => setSideNav(true)}>
+          <FiMenu className="text-2xl cursor-pointer" />
+        </div>
+        <div>
+          <Link className="text-2xl font-semibold" href="/">
+            GOODLITE
+          </Link>
+        </div>
+        <div
+          className="relative cursor-pointer"
+          onClick={() => setCartOpen(true)}
+        >
+          <Image src="/cart.svg" alt="Cart SVG" height={30} width={30} />
+          <div className="absolute top-0 left-2 bg-black text-white rounded-full border-2 border-black h-4 w-4 flex items-center justify-center pointer-events-none">
+            <span className="text-xs pointer-events-none">0</span>
           </div>
-          <div>
-            <Link className="text-2xl font-semibold" href="/">
-              GOODLITE
-            </Link>
+        </div>
+      </div>
+      <div className="hidden lg:flex w-full bg-white lg:items-center lg:justify-between lg:px-8 lg:py-5">
+        <div className="">
+          <Link className="text-2xl font-semibold" href="/">
+            GOODLITE
+          </Link>
+        </div>
+        <div>
+          <ul className="flex gap-6 text-lg">
+            <li>
+              <Link
+                href="/"
+                className="lg:text-sm xl:text-base border-a-expand"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="lg:text-sm xl:text-base border-a-expand"
+              >
+                Categories
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="lg:text-sm xl:text-base border-a-expand"
+              >
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="lg:text-sm xl:text-base border-a-expand"
+              >
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex items-center lg:gap-2 xl:gap-4">
+          <div className="flex xl:gap-2 items-center border-2">
+            <input
+              type="search"
+              className="px-4 py-2 w-9/12 focus:border-0 outline-none"
+              placeholder="Search products..."
+            />
+            <FiSearch className="text-2xl cursor-pointer" />
           </div>
           <div
             className="relative cursor-pointer"
@@ -40,62 +99,33 @@ const Navbar: FC = () => {
               <span className="text-xs pointer-events-none">0</span>
             </div>
           </div>
-        </div>
-      ) : (
-        <div className="flex flex-col w-full p-5 bg-white md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
-          <div className="">
-            <Link className="text-2xl font-semibold" href="/">
-              GOODLITE
-            </Link>
-          </div>
-          <div>
-            <ul className="flex gap-6 text-lg">
-              <li>
-                <Link href="/" className="border-a-expand">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link href="/" className="border-a-expand">
-                  Categories
-                </Link>
-              </li>
-              <li>
-                <Link href="/" className="border-a-expand">
-                  Products
-                </Link>
-              </li>
-              <li>
-                <Link href="/" className="border-a-expand">
-                  About
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="flex items-center gap-4">
-            <div className="flex gap-2 items-center border-2">
-              <input
-                type="search"
-                className="px-4 py-2 w-9/12 focus:border-0 outline-none"
-                placeholder="Search products..."
-              />
-              <FiSearch className="text-2xl cursor-pointer" />
-            </div>
-            <div
-              className="relative cursor-pointer"
-              onClick={() => setCartOpen(true)}
+          {!session ? (
+            <button
+              className="inline-flex items-center justify-center px-4 py-2 text-sm font-semibold text-white
+               bg-black rounded-full group focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2
+                hover:bg-gray-700 active:bg-gray-800 active:text-white focus-visible:outline-black"
+              onClick={() => signIn()}
             >
-              <Image src="/cart.svg" alt="Cart SVG" height={30} width={30} />
-              <div className="absolute top-0 left-2 bg-black text-white rounded-full border-2 border-black h-4 w-4 flex items-center justify-center pointer-events-none">
-                <span className="text-xs pointer-events-none">0</span>
-              </div>
-            </div>
-            <button className="inline-flex items-center justify-center px-4 py-2 text-sm font-semibold text-white bg-black rounded-full group focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 hover:bg-gray-700 active:bg-gray-800 active:text-white focus-visible:outline-black">
               Sign up
             </button>
-          </div>
+          ) : (
+            <div className="flex items-center cursor-pointer">
+              <div>
+                <Image
+                  src={session?.user?.image ?? "/user-icon.png"}
+                  alt={"User Icon"}
+                  height={40}
+                  width={40}
+                  className="rounded-full p-1"
+                />
+              </div>
+              <h1 className="text-sm font-medium border-a-expand">
+                Hi, {session?.user?.name}
+              </h1>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
--- context/context.tsx
+++ context/context.tsx
@@ -3,8 +3,6 @@ import { createContext, useContext, useMemo, useState } from "react";
 interface ContextType {
   cartOpen: boolean;
   setCartOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  isMobile: boolean;
-  setIsMobile: React.Dispatch<React.SetStateAction<boolean>>;
   sideNav: boolean;
   setSideNav: React.Dispatch<React.SetStateAction<boolean>>;
 }
@@ -16,8 +14,6 @@ interface Props {
 const defaultContext: ContextType = {
   cartOpen: false,
   setCartOpen: () => {},
-  isMobile: false,
-  setIsMobile: () => {},
   sideNav: false,
   setSideNav: () => {},
 };
@@ -26,19 +22,16 @@ const Context = createContext<ContextType>(defaultContext);
 
 export const ContextProvider: React.FC<Props> = ({ children }: Props) => {
   const [cartOpen, setCartOpen] = useState<boolean>(false);
-  const [isMobile, setIsMobile] = useState<boolean>(false);
   const [sideNav, setSideNav] = useState<boolean>(false);
 
   const contextValue: ContextType = useMemo(
     () => ({
       cartOpen,
       setCartOpen,
-      isMobile,
-      setIsMobile,
       sideNav,
       setSideNav,
     }),
-    [cartOpen, isMobile, sideNav]
+    [cartOpen, sideNav]
   );
 
   return <Context.Provider value={contextValue}>{children}</Context.Provider>;
